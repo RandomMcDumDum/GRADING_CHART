bcefc6104ea589e0c3d93fb59b9edf97bc2455ed
Christian Rodriguez <cgrod12@gmail.com>
2023-05-16 17:52:27 -0500

    Created /views and placed index.ejs within it to be rendered by the server.

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const path = require('path');
 const app = express()
 const PORT = 5000
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // Send index HTML file
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '/index.html'))
+// })
+
+// Respond with Rendered EJS
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.render('index.ejs')
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
